4fbfbac3757509c1ca3b3651e407dd5f55fe4fc6
maanasamalar <15eucs067@skcet.ac.in>
2018-01-04 22:16:58 +0530

    Create bitonic_max.c

---
+++ C PROGRAMS/bitonic_max.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int a[50],n,i,max,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	max=a[0];
+	for(i=1;i<n;i++)
+	{
+	  
+	   if(a[i]>max)
+	   {
+	       max=a[i];
+	   }
+	 
+	}
+	for(i=1;i<n;i++)
+	{
+	   
+	if(max>a[i])
+	    break;
+	}
+	printf("%d",max);
+	
+	return 0;
+}
