1274762abf07dc0b3c72de80764ae561a3ac0153
maanasamalar <15eucs067@skcet.ac.in>
2018-01-08 12:24:58 +0530

    Create uprcase_or_lwrcase_with_position.c

---
+++ Guvi/uprcase_or_lwrcase_with_position.c
@@ -0,0 +1,51 @@
+#include <stdio.h> 
+#include<string.h>
+ 
+ int main() { 
+ char str[20];
+   int i,n,t,p,v,u;
+   scanf("%s",str);
+   scanf("%d",&t);
+   scanf("%d",&p);
+   n=strlen(str);
+  
+     if(t==1)
+     {
+       for(i=p-1;i<n;i=i+p+i)
+       {
+         u=str[i];
+       if(u<97)
+       {
+         v=u+32;
+         str[i]=v;
+     }
+        
+
+       }
+        printf("%s",str);
+     }
+     if(t==2)
+     {
+        for(i=p-1;i<n;i=i+p)
+       {
+         u=str[i];
+       if(u>91)
+       {
+         v=u-32;
+         str[i]=v;
+     }
+        
+        }
+         printf("%s",str);
+     }
+   
+   
+     
+     
+   
+  
+     
+   
+   
+ return 0; 
+}
