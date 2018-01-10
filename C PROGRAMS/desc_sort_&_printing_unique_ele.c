e37cf5f8e406f935aeec0a26aed9fc7cc4cac77e
maanasamalar <15eucs067@skcet.ac.in>
2018-01-10 20:36:08 +0530

    Create desc_sort_&_printing_unique_ele.c

---
+++ C PROGRAMS/desc_sort_&_printing_unique_ele.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+	int a[100],i,j,temp,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+	    for(j=i+1;j<n;j++)
+	    {
+	        if(a[i]<a[j])
+	        {
+	           temp=a[i];
+	           a[i]=a[j];
+	           a[j]=temp;
+	        }
+	    }
+	}
+	/*for(i=0;i<n;i++)
+	{
+	    printf("%d ",a[i]);
+	}*/
+	for(i=0;i<n;i++)
+	{
+	    for(j=0;j<i;j++)
+	    {
+	        if(a[i]==a[j])
+	        {
+	            break;
+	        }
+	    }
+	    if(i==j)
+	    {
+	        printf("%d ",a[i]);
+	    }
+	}
+	
+	return 0;
+}
