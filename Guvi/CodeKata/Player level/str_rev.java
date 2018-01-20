7d68b48829786bd252487dc0e4bb2ebf63740ee2
maanasamalar <15eucs067@skcet.ac.in>
2018-01-20 04:41:06 +0000

    Create str_rev.java

---
+++ Guvi/CodeKata/Player level/str_rev.java
@@ -0,0 +1,15 @@
+
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class string_rev{
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    StringBuffer sb=new StringBuffer(sc.nextLine());
+	    StringBuffer rev=sb.reverse();
+	    System.out.println(rev);
+	    
+
+	}
+}
